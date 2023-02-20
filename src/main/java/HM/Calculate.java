a3e51d75bc4a67357ac9956b8372a74222d5232c
OlegBrozhyna <122048830+OlegBrozhyna@users.noreply.github.com>
2023-02-20 11:00:20 +0200

    Update Calculate.java

--- src/main/java/HM/Calculate.java
+++ src/main/java/HM/Calculate.java
@@ -10,20 +10,20 @@ public class Calculate {
     public static void main(String[] args) {
         Set<Method> annotatedMethods = new HashSet<>();
         Set<Parameter> annotatedParameters = new HashSet<>();
-        //Перебирає класи в ієрархії
+    
         for (Class<?> clazz = MethodAnnotation.class;clazz !=null; clazz.getSuperclass()){
-           // Перебір методів у кожному класі для
-            for (Method method:clazz.getDeclaredMethods()) {
-             //   Перевіряє , чи метод анотовано @MyAnnotation
+           for (Method method:clazz.getDeclaredMethods()) {
+            
+             
                 if (method.isAnnotationPresent(MyAnnotation.class)){
-                    //Перебирає параметри у методі
-                    annotatedMethods.add(method);
+                     annotatedMethods.add(method);
+                  
 
                 }
                 for (Parameter parameter: method.getParameters()){
-                    // Перевіряє, чи параметр анотовано за допомогою @MyAnnotation
                     if (parameter.isAnnotationPresent(MyAnnotation.class)){
                         annotatedParameters.add(parameter);
+                    
 
                         }
                     }
